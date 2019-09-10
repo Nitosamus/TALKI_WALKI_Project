03d90e9c4ae6ad460a00bc0e4be04bb54eaaea88
Tonie <toniemontfort@gmail.com>
2019-09-10 09:37:11 +0300

    premier lancement sur heroku

---
+++ db/migrate/20190910062430_add_column_to_user.rb
@@ -0,0 +1,11 @@
+class AddColumnToUser < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :city, :string
+    add_column :users, :zip_code, :integer
+    add_column :users, :description, :text
+    add_column :users, :function, :string
+    add_column :users, :phone_number, :string
+  end
+end
