60e6e102073b2802cd29cc68990ccb5daf547cfd
Mandresy4 <harenamandresy04@gmail.com>
2019-09-11 16:01:47 +0300

    creation table F_A et table O_E

---
+++ db/migrate/20190911130012_create_offre_emplois.rb
@@ -0,0 +1,12 @@
+class CreateOffreEmplois < ActiveRecord::Migration[5.2]
+  def change
+    create_table :offre_emplois do |t|
+      t.string :description
+      t.string :lieu
+      t.string :salaire
+      t.string :mail
+
+      t.timestamps
+    end
+  end
+end
