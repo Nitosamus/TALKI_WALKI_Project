bcfee20a8f80320e744ce2e0f42a01d47fd3655f
Tonie <toniemontfort@gmail.com>
2019-09-14 04:54:30 +0300

    front login singn up

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,7 @@ ProfessionalFormation.destroy_all
 User.destroy_all
 OffreEmploi.destroy_all
 FormationAcademique.destroy_all
+
 Place.create!([
 { "name": "Buckingham Palace", "latitude": "51.501564","longitude": "-0.141944"},
 { "name": "Westminster Abbey", "latitude": "51.499581", "longitude": "-0.127309"},
