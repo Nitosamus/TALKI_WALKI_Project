f6434a80778ef4a84b8decd6d53f4261e1f579ed
Andrygaby <angeg7360@gmail.com>
2019-09-16 07:59:25 +0300

    front

--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,10 @@
-Rails.application.routes.draw do
 
-  resources :professional_formations do
-    resources :commentaires do 
-      resources :reponses
-    end
-  end
-  
-  resources :offre_emplois do
-    resources :commentaires do 
-      resources :reponses
-    end
-  end
-
-  resources :formation_academiques  do
-    resources :commentaires do 
-      resources :reponses
-    end
-  end
-
-  resources :domaines
-  resources :places
-  root to: "publications#index"
-  devise_for :users
-  
-  resources :users do
- 	  resources :images, only:[:create]
-  end
+ 	resources :images, only:[:create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  end
+  resources :places
+  resources :users do
+  root to: "publications#index"
 end
+Rails.application.routes.draw do
