7854ad0c65c434181c9cff67eb9850d3fd292fc8
Tamby02 <ihtambyrak@gmail.com>
2019-09-17 11:18:30 +0300

    db au final

--- app/controllers/reponse_academiques_controller.rb
+++ app/controllers/reponse_academiques_controller.rb
@@ -10,12 +10,20 @@ class ReponseAcademiquesController < ApplicationController
   end
 
   def edit
-    @reponse= Reponse.find[:id]
+    @reponse= Reponse.find(params[:id])
   end
 
   def update
+    @reponses= Reponse.find(params[:id])
+    if @reponse= @reponses.update(user_id: current_user.id, contenu: params[:contenu],  commentaire_id: params[:commentaire_academique_id])
+       redirect_to formation_academique_path(params[:formation_academique_id])
+     end
   end
 
   def destroy
+    @reponse= Reponse.find(params[:id])
+    if @reponse= Reponse.destroy(params[:id])
+      redirect_to formation_academique_path(params[:formation_academique_id])
+    end
   end
 end
