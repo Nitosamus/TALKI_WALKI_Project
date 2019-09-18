4e30a1207c40b1f2a5f1567672002c16bf94998c
Andrygaby <angeg7360@gmail.com>
2019-09-18 09:12:15 +0300

    front

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
- 	Rails.application.routes.draw do
+Rails.application.routes.draw do
   resources :images, only:[:create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
@@ -32,46 +32,12 @@
       end
     end
   end
-<<<<<<< HEAD
-  resources :places
-  root to: "publications#index"
-||||||| merged common ancestors
-<<<<<<< HEAD
-
-  resources :domaines
-
-  resources :places
-||||||| merged common ancestors
-  resources :places
-=======
-
-    resources :places
-    resources :users do
-    root to: "publications#index"
-
->>>>>>> front_accueil
-  resources :users do
- 	  resources :images, only:[:create]
-
-  resources :places
-  root to: "publications#index"
-  devise_for :users
-  
-=======
 
 
     resources :places
     resources :users 
     root to: "publications#index"
->>>>>>> 9ccd6f38c73697d269a3fc76f54f132d220a32a5
   resources :users do
- 	  resources :images, only:[:create]
-<<<<<<< HEAD
-  end
-
-||||||| merged common ancestors
-  end
-=======
+    resources :images, only:[:create]
 end
->>>>>>> 9ccd6f38c73697d269a3fc76f54f132d220a32a5
 end
