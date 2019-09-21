4118ed8274409d04ea7f36632cdd55e1bdce726f
Tamby02 <52703927+Tamby02@users.noreply.github.com>
2019-09-21 05:33:16 +0300

    Update like_professionels_controller.rb

--- app/controllers/like_professionels_controller.rb
+++ app/controllers/like_professionels_controller.rb
@@ -1,17 +1,25 @@
 class LikeProfessionelsController < ApplicationController
   def new
-  	@like= Like.new( user_id: current_user.id, commentaire_id: params[:commentaire_professionel_id], reponse_id:  params[:reponse_professionnel_id], professional_formation_id:  params[:professional_formation_id])
+  	@like= Like.new( user_id: current_user.id, professional_formation_id:  params[:professional_formation_id])
   end
 
   def create
-  	@like= Like.create( user_id: current_user.id, commentaire_id: params[:commentaire_professionel_id], reponse_id:  params[:reponse_professionnel_id], professional_formation_id:  params[:professional_formation_id])
-  	 
-  	ProfessionalFormation.find(params[:professional_formation_id]).likes<< @like
-  	Commentaire.find(params[:commentaire_professionel_id]).likes<<@like
-  	Reponse.find(params[:reponse_professionnel_id]).likes << @like
-  	redirect_back fallback_location: '/', allow_other_host: false
+  	
+  	 if interresse == 0 
+  	 	@like= Like.create( user_id: current_user.id, professional_formation_id:  params[:professional_formation_id])
+  	 	ProfessionalFormation.find(params[:professional_formation_id]).likes<<@like
+  	 	redirect_back fallback_location: '/', allow_other_host: false
+ 	else interresse> 1  || interresse==1
+ 		@likes=Like.find_by(user_id: current_user)
+ 		ProfessionalFormation.find(params[:professional_formation_id]).likes.delete(@likes)
+ 		redirect_back fallback_location: '/', allow_other_host: false
+ 	end
+	
   end
 
-  def destroy
+  private
+  def interresse
+  	ProfessionalFormation.find(params[:professional_formation_id]).likes.count 
   end
+
 end
