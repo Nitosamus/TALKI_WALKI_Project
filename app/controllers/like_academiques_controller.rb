4534a8dc04e7f22c2a9304f80cc3acda180382c2
Tamby02 <52703927+Tamby02@users.noreply.github.com>
2019-09-21 05:31:23 +0300

    Update like_academiques_controller.rb

--- app/controllers/like_academiques_controller.rb
+++ app/controllers/like_academiques_controller.rb
@@ -1,17 +1,24 @@
 class LikeAcademiquesController < ApplicationController
   def new
-  	@like= Like.new( user_id: current_user.id, commentaire_id: params[:commentaire_academique_id], reponse_id:  params[:reponse_academique_id], formation_academique_id:  params[:formation_academique_id])
+  	@like= Like.new( user_id: current_user.id, formation_academique_id:  params[:formation_academique_id])
   end
 
   def create
-  	@like= Like.create( user_id: current_user.id, commentaire_id: params[:commentaire_academique_id], reponse_id:  params[:reponse_academique_id], )
-  	@likes= Like.create( user_id: current_user.id, formation_academique_id:  params[:formation_academique_id])
-  	FormationAcademique.find(params[:formation_academique_id]).likes<< @likes
-  	Commentaire.find(params[:commentaire_academique_id]).likes<<@like
-  	Reponse.find(params[:reponse_academique_id]).likes << @like
-  	redirect_back fallback_location: '/', allow_other_host: false
+  	
+  	 if interresse == 0 
+  	 	@like= Like.create( user_id: current_user.id, formation_academique_id:  params[:formation_academique_id])
+  	 	FormationAcademique.find(params[:formation_academique_id]).likes<<@like
+  	 	redirect_back fallback_location: '/', allow_other_host: false
+ 	else interresse> 1  || interresse==1
+ 		@likes=Like.find_by(user_id: current_user)
+ 		FormationAcademique.find(params[:formation_academique_id]).likes.delete(@likes)
+ 		redirect_back fallback_location: '/', allow_other_host: false
+ 	end
+	
   end
 
-  def destroy
+  private
+  def interresse
+  	FormationAcademique.find(params[:formation_academique_id]).likes.count 
   end
 end
