dac0fcf09c948184ddabdfc7aad8fac3ad688659
Tonie <toniemontfort@gmail.com>
2019-09-21 17:38:53 +0300

    m

--- db/schema.rb
+++ db/schema.rb
@@ -75,7 +75,6 @@ ActiveRecord::Schema.define(version: 2019_09_18_144350) do
     t.string "mail"
     t.string "lieu"
     t.text "description"
-    t.string "domaine"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -104,7 +103,6 @@ ActiveRecord::Schema.define(version: 2019_09_18_144350) do
     t.string "lieu"
     t.string "salaire"
     t.string "mail"
-    t.string "domaine"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -161,7 +159,6 @@ ActiveRecord::Schema.define(version: 2019_09_18_144350) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
-    t.string "username", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -176,10 +173,10 @@ ActiveRecord::Schema.define(version: 2019_09_18_144350) do
     t.text "description"
     t.string "function"
     t.string "phone_number"
+    t.string "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["room_id"], name: "index_users_on_room_id"
-    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
