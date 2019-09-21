40a7fcfb1ffa3cffa182bccb596e8b0ae30ab4d7
Tamby02 <52703927+Tamby02@users.noreply.github.com>
2019-09-21 05:32:37 +0300

    Update like_emplois_controller.rb

--- app/controllers/like_emplois_controller.rb
+++ app/controllers/like_emplois_controller.rb
@@ -1,16 +1,24 @@
 class LikeEmploisController < ApplicationController
-  def new
-  	@like= Like.new( user_id: current_user.id, commentaire_id: params[:commentaire_emploi_id], reponse_id:  params[:reponse_emploi_id], offre_emploi_id:  params[:offre_emploi_id])
+	def new
+  	@like= Like.new( user_id: current_user.id, offre_emploi_id:  params[:offre_emploi_id])
   end
 
   def create
-  	@like= Like.create( user_id: current_user.id, commentaire_id: params[:commentaire_emploi_id], reponse_id:  params[:reponse_emploi_id], offre_emploi_id:  params[:offre_emploi_id])
-  	OffreEmploi.find(params[:offre_emploi_id]).likes<< @like
-  	Commentaire.find(params[:commentaire_emploi_id]).likes<<@like
-  	Reponse.find(params[:reponse_emploi_id]).likes << @like
-  	redirect_back fallback_location: '/', allow_other_host: false
+  	
+  	 if interresse == 0 
+  	 	@like= Like.create( user_id: current_user.id, offre_emploi_id:  params[:offre_emploi_id])
+  	 	OffreEmploi.find(params[:offre_emploi_id]).likes<<@like
+  	 	redirect_back fallback_location: '/', allow_other_host: false
+ 	else interresse> 1  || interresse==1
+ 		@likes=Like.find_by(user_id: current_user)
+ 		OffreEmploi.find(params[:offre_emploi_id]).likes.delete(@likes)
+ 		redirect_back fallback_location: '/', allow_other_host: false
+ 	end
+	
   end
 
-  def destroy
+  private
+  def interresse
+  	OffreEmploi.find(params[:offre_emploi_id]).likes.count 
   end
 end
