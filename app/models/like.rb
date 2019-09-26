6739406ec4b583f81734a1131249269e29527427
Tamby02 <52703927+Tamby02@users.noreply.github.com>
2019-09-26 14:45:03 +0200

    Delete like.rb

--- app/models/like.rb
+++
@@ -1,8 +0,0 @@
-class Like < ApplicationRecord
-	belongs_to :user
-	belongs_to :commentaire , optional:true
-	belongs_to :reponse , optional:true
-	belongs_to :formation_academique, optional: true
-	belongs_to :professional_formation, optional: true
-	belongs_to :offre_emploi, optional: true
-end
