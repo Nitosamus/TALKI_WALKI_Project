088a50c97df6ec4301eebebc2a0dbe1c88cb18d2
Tamby02 <52703927+Tamby02@users.noreply.github.com>
2019-09-26 14:45:00 +0200

    Delete formation_academique.rb

--- app/models/formation_academique.rb
+++
@@ -1,9 +0,0 @@
-class FormationAcademique < ApplicationRecord
-	belongs_to :user
-	has_many :commentaires
-	has_many :likes
-
-	validates :titre, presence: true
-	validates :description, presence: true
-	validates :mail, presence: true
-end
